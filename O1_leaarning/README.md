a173a11ca72a7fb83d4341b79b8a55b232bcfab7
Hadi493 <hadialam493@gmail.com>
2024-12-23 15:15:18 +0600

    started pattern printing

--- O1_leaarning/README.md
+++ O1_leaarning/README.md
@@ -1,1 +1,7 @@
 ## Learning DSA With C++ By Love babbar
+
+- flow chart
+- Syntex
+- Variables & Data Types
+- Conditional-(If-else)
+- Loops
--- O1_leaarning/main.cpp
+++ O1_leaarning/main.cpp
@@ -3,6 +3,29 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << std::endl;
+    int n;
+    cout << "Enter a number :: ";
+    cin >> n;
+
+    int row = 1;
+    // while (row <= n) {
+    //     int col = 1;
+
+    //     while (col <= row) {
+    //         cout << "#";
+    //         col++;
+    //     }
+    //     cout << endl;
+    //     row++;
+    // }
+
+    for (int row = 1; row <= n; row++) {
+        for (int col = 1; col <= row; col++) {
+            cout << "#";
+        }
+        cout << endl;
+    }
+
+
     return 0;
 }
